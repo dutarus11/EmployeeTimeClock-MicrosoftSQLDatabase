30a11fc30ad2217becf7b7c05d4e16ff24f4dd4e
dutarus11 <dutarus@gmail.com>
2022-04-21 19:57:45 -0700

    Add project files.

---
+++ EmployeeTimeClock/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace EmployeeTimeClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.clockIN = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.clockOUT = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(328, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(26, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter EmployeeID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(322, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time Clock:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clock In ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(505, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clock Out";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(471, 263);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(80, 25);
+            this.lblStatus.TabIndex = 7;
+            this.lblStatus.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 31);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 138);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 31);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(33, 110);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(166, 25);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Employee Name: ";
+            // 
+            // clockIN
+            // 
+            this.clockIN.Location = new System.Drawing.Point(322, 138);
+            this.clockIN.Name = "clockIN";
+            this.clockIN.Size = new System.Drawing.Size(150, 31);
+            this.clockIN.TabIndex = 13;
+           
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(320, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(115, 25);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Clocked In: ";
+            // 
+            // clockOUT
+            // 
+            this.clockOUT.Location = new System.Drawing.Point(495, 138);
+            this.clockOUT.Name = "clockOUT";
+            this.clockOUT.Size = new System.Drawing.Size(150, 31);
+            this.clockOUT.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label10.Location = new System.Drawing.Point(495, 110);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(131, 25);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Clocked Out: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(966, 422);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.clockOUT);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.clockIN);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox clockIN;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox clockOUT;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ EmployeeTimeClock/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+ 
+namespace EmployeeTimeClock//Employee Time Clock Script
+{
+    public partial class Form1 : Form
+    {
+        int empID;
+        DateTime beginTime = DateTime.Now;
+        DateTime endTime = DateTime.Now;
+             
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();// Timer Method 
+        }
+        
+             
+        private void timer1_Tick(object sender, EventArgs e)//Timer Logic 
+        {
+            lblStatus.Text = DateTime.Now.ToString("T");
+        }
+
+      
+        private void button1_Click(object sender, EventArgs e) //Clock In Logic 
+        {
+            beginTime = DateTime.Now;
+            clockIN.Text = beginTime.ToShortTimeString();           
+           
+            SqlConnection conn1 = new SqlConnection(); //SQL Connection Logic 
+            conn1.ConnectionString = "Data Source=DESKTOP-9IKPMC3\\GOBERTUTORIALSQL;Initial Catalog=empTimeClockDataBase;Integrated Security=True"; 
+            conn1.Open();
+            SqlCommand cmd = new SqlCommand("select id, employeename from data where id=@id " , conn1);
+            cmd.Parameters.AddWithValue("id", textBox2.Text);
+            SqlDataReader reader1;
+            reader1 = cmd.ExecuteReader();
+            
+            if (reader1.Read())
+            {
+                textBox2.Text = reader1["employeename"].ToString();
+
+                if (beginTime.Hour == 8) //on time
+                {
+                    MessageBox.Show("Congrats! You clocked in on time.:) ");
+                }
+                else if (beginTime.Hour > 8 && beginTime.Hour < 11)
+                {
+                    MessageBox.Show("You are late! ");
+                }
+               
+                else if (beginTime.Hour > 11 || beginTime.Hour < 8 && endTime.Hour != 12)
+                {
+                    MessageBox.Show("Clocking In Early Not Permitted! ");
+                }
+            }
+            else
+            {
+                MessageBox.Show("NO DATA FOUND");
+            }
+            
+            conn1.Close();
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)// Clock Out Logic 
+        {
+            endTime = DateTime.Now;
+            clockOUT.Text = endTime.ToShortTimeString();
+            if (endTime.Hour < 4 && beginTime.Hour > 2) //too early
+            {
+                MessageBox.Show("You punched out early! ");
+            }
+            if (endTime.Hour == 5) //on time
+            {
+                MessageBox.Show("Congrats! You clocked out on time.:) ");
+            }          
+        }
+
+        
+    }
+}
